9731f096b1fe0a92184ed08ab9ec81d55b8e4587
ccld <161834186+ccldrepo@users.noreply.github.com>
2024-05-03 11:46:08 +0800

    Update code base

--- src/PCH.h
+++ src/PCH.h
@@ -1,31 +1,41 @@
 #pragma once
 
 #include <algorithm>
+#include <any>
 #include <array>
 #include <atomic>
 #include <bit>
+#include <cassert>
 #include <cctype>
+#include <cerrno>
 #include <chrono>
 #include <climits>
 #include <compare>
 #include <concepts>
 #include <cstddef>
 #include <cstdint>
+#include <cstdio>
+#include <cstdlib>
 #include <cstring>
 #include <exception>
+#include <execution>
 #include <filesystem>
 #include <format>
 #include <fstream>
 #include <functional>
+#include <future>
 #include <initializer_list>
 #include <ios>
+#include <istream>
 #include <map>
 #include <memory>
 #include <mutex>
 #include <optional>
+#include <ostream>
 #include <ranges>
 #include <set>
 #include <shared_mutex>
+#include <source_location>
 #include <span>
 #include <sstream>
 #include <stdexcept>
@@ -38,7 +48,9 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <utility>
+#include <variant>
 #include <vector>
+#include <version>
 
 #include <RE/Skyrim.h>
 #include <REL/Relocation.h>
@@ -50,15 +62,45 @@
 #include <fmt/format.h>
 #include <fmt/ranges.h>
 
+#include <spdlog/spdlog.h>
+
 using namespace std::literals::string_view_literals;
 
 namespace SKSE::stl
 {
-    template <class T>
-    inline void write_thunk_call(REL::Relocation<std::uintptr_t> a_src)
+    class log_message
     {
-        SKSE::AllocTrampoline(14);
-        T::func = a_src.write_call<5>(T::tunk);
+    public:
+        explicit log_message(std::string a_msg) : msg(std::move(a_msg)) {}
+
+        const std::string& string() const noexcept { return msg; }
+        const char*        c_str() const noexcept { return msg.c_str(); }
+        std::size_t        size() const noexcept { return msg.size(); }
+
+    private:
+        std::string msg;
+    };
+
+    inline void log_success(std::string a_msg, bool a_throw,
+        std::source_location a_loc = std::source_location::current())
+    {
+        spdlog::log(spdlog::source_loc{ a_loc.file_name(), static_cast<int>(a_loc.line()), a_loc.function_name() },
+            spdlog::level::info, a_msg);
+        if (a_throw) {
+            throw SKSE::stl::log_message(std::move(a_msg));
+        }
+    }
+
+    [[noreturn]] inline void log_failure(std::string a_msg, bool a_throw,
+        std::source_location a_loc = std::source_location::current())
+    {
+        if (!a_throw) {
+            SKSE::stl::report_and_fail(a_msg, a_loc);
+        } else {
+            spdlog::log(spdlog::source_loc{ a_loc.file_name(), static_cast<int>(a_loc.line()), a_loc.function_name() },
+                spdlog::level::err, a_msg);
+            throw SKSE::stl::log_message(std::move(a_msg));
+        }
     }
 }
 
